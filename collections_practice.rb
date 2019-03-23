b7a5e79e72255d9aa75d074505171acb2e66f70e
Patrick A <akolo@hotmail.com>
2019-03-23 13:20:39 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,7 +1,7 @@
-def sort_array_asc (sort_array_asc)
+def sort_array_asc (sort_array_asc, sort_array_desc)
   sort_array_asc.sort 
   end 
 
-  sort_array_asc.sort_by {|a,b| b <= a}
+  sort_array_desc.sort_by {|a,b| b <= a}
   end
 end 
