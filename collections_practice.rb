d39929cb382351e30ddb4bce535a368cdb6ba48d
Patrick A <akolo@hotmail.com>
2019-03-23 13:38:59 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,6 +1,6 @@
 def collections_practice (sort_array_asc, sort_array_desc)
   sort_array_asc.sort 
   end 
-  sort_array_desc.sort_by {|num| -num }
+  sort_array_desc.sort_by {|num| num.reverse }
   end 
 end
