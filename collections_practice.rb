2ad9da374109eefafa129a7785a11b52533c01b3
Patrick A <akolo@hotmail.com>
2019-03-23 14:15:25 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -8,8 +8,8 @@ def sort_array_desc (array)
   end
 end
 
-sort_array_char_count(["dogs", "cat", "Horses"])
 def sort_array_char_count(array)
+  array =["dogs", "cat", "Horses"]
   array.sort do |a,b| a.length <=> b.length
   
   #|a,b| a.length <=> b.length
