5d340bd60f93c912cc3c93ac364bb72e05fb335e
Patrick A <akolo@hotmail.com>
2019-03-23 14:19:40 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,12 +5,13 @@ end
 def sort_array_desc (array)
   array = [25, 7, 14]
   array.sort {|a,b| b <=> a}
-  end
+end
 
 def sort_array_char_count(array)
   array =["dogs", "cat", "Horses"]
   array.sort do |a,b| a.length <=> b.length
-  
-  #|a,b| a.length <=> b.length
   end 
 end
+
+def swap_elements(array)
+  array = 
